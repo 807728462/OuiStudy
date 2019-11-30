3e1443b9b216a3d40547ccfe9b5026302ec40589
pengbin <514594945@qq.com>
2019-11-30 12:03:53 +0800

    zhujiemian

---
+++ app/src/main/java/com/oyf/codecollection/ui/contract/LoginContract.java
@@ -0,0 +1,14 @@
+package com.oyf.codecollection.ui.contract;
+
+import com.oyf.basemodule.mvp.BaseModel;
+
+/**
+ * @创建者 oyf
+ * @创建时间 2019/11/28 12:06
+ * @描述
+ **/
+public class LoginContract {
+    interface LoginModel{
+
+    }
+}
